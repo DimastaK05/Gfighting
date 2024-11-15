752c2e7f8c41ada94ecdc80d76dfdf8761747293
fezonV <66324221+fezonV@users.noreply.github.com>
2024-11-15 20:57:25 +0300

    сделал главное меню с рабочими кнопками

---
+++ Gfighting/Assets/Scenes/Mainmenu/Scripts/SceneManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+}
